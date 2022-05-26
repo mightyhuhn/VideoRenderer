106756a57b1d902623c2d7b98924002309431b21
huhn mighty <mightyhuhn@gmail.com>
2022-05-27 01:38:01 +0200

    jinc changes

--- Shaders/examples/ps_resize_onepass_jinc2.hlsl
+++ Shaders/examples/ps_resize_onepass_jinc2.hlsl
@@ -16,16 +16,16 @@ struct PS_INPUT
 };
 
 #if 1
-#define JINC2_WINDOW_SINC 0.405
-#define JINC2_SINC        0.79
-#define JINC2_AR_STRENGTH 0.8
+#define JINC2_WINDOW_SINC 0.38
+#define JINC2_SINC        0.90
+#define JINC2_AR_STRENGTH 0.0
 #else
 #define JINC2_WINDOW_SINC 0.44
-#define JINC2_SINC        0.82
+#define JINC2_SINC        0.77
 #define JINC2_AR_STRENGTH 0.5
 #endif
 
-static const float pi = acos(-1);
+static const float pi = 3.44;
 static const float wa = JINC2_WINDOW_SINC * pi;
 static const float wb = JINC2_SINC * pi;
 
@@ -55,10 +55,10 @@ float4 main(PS_INPUT input) : SV_Target
     float2 tc = floor(pc - 0.5) + 0.5;
 
     float4x4 weights = {
-        resampler(float4(d(pc, tc - dx - dy  ), d(pc, tc - dy  ), d(pc, tc + dx - dy  ), d(pc, tc + 2*dx - dy  ))),
-        resampler(float4(d(pc, tc - dx       ), d(pc, tc       ), d(pc, tc + dx       ), d(pc, tc + 2*dx       ))),
-        resampler(float4(d(pc, tc - dx + dy  ), d(pc, tc + dy  ), d(pc, tc + dx + dy  ), d(pc, tc + 2*dx + dy  ))),
-        resampler(float4(d(pc, tc - dx + 2*dy), d(pc, tc + 2*dy), d(pc, tc + dx + 2*dy), d(pc, tc + 2*dx + 2*dy)))
+        resampler(float4(d(pc, tc - dx - dy), d(pc, tc - dy), d(pc, tc + dx - dy), d(pc, tc + 2 * dx - dy))),
+        resampler(float4(d(pc, tc - dx), d(pc, tc), d(pc, tc + dx), d(pc, tc + 2 * dx))),
+        resampler(float4(d(pc, tc - dx + dy), d(pc, tc + dy), d(pc, tc + dx + dy), d(pc, tc + 2 * dx + dy))),
+        resampler(float4(d(pc, tc - dx + 2 * dy), d(pc, tc + 2 * dy), d(pc, tc + dx + 2 * dy), d(pc, tc + 2 * dx + 2 * dy)))
     };
 
     dx *= dxdy;
@@ -70,22 +70,22 @@ float4 main(PS_INPUT input) : SV_Target
     // [ c01, c11, c21, c31 ]
     // [ c02, c12, c22, c32 ]
     // [ c03, c13, c23, c33 ]
-    float3 c00 = tex.Sample(samp, tc - dx   - dy  ).rgb;
-    float3 c10 = tex.Sample(samp, tc        - dy  ).rgb;
-    float3 c20 = tex.Sample(samp, tc + dx   - dy  ).rgb;
-    float3 c30 = tex.Sample(samp, tc + 2*dx - dy  ).rgb;
-    float3 c01 = tex.Sample(samp, tc - dx         ).rgb;
-    float3 c11 = tex.Sample(samp, tc              ).rgb;
-    float3 c21 = tex.Sample(samp, tc + dx         ).rgb;
-    float3 c31 = tex.Sample(samp, tc + 2*dx       ).rgb;
-    float3 c02 = tex.Sample(samp, tc - dx   + dy  ).rgb;
-    float3 c12 = tex.Sample(samp, tc        + dy  ).rgb;
-    float3 c22 = tex.Sample(samp, tc + dx   + dy  ).rgb;
-    float3 c32 = tex.Sample(samp, tc + 2*dx + dy  ).rgb;
-    float3 c03 = tex.Sample(samp, tc - dx   + 2*dy).rgb;
-    float3 c13 = tex.Sample(samp, tc        + 2*dy).rgb;
-    float3 c23 = tex.Sample(samp, tc + dx   + 2*dy).rgb;
-    float3 c33 = tex.Sample(samp, tc + 2*dx + 2*dy).rgb;
+    float3 c00 = tex.Sample(samp, tc - dx - dy).rgb;
+    float3 c10 = tex.Sample(samp, tc - dy).rgb;
+    float3 c20 = tex.Sample(samp, tc + dx - dy).rgb;
+    float3 c30 = tex.Sample(samp, tc + 2 * dx - dy).rgb;
+    float3 c01 = tex.Sample(samp, tc - dx).rgb;
+    float3 c11 = tex.Sample(samp, tc).rgb;
+    float3 c21 = tex.Sample(samp, tc + dx).rgb;
+    float3 c31 = tex.Sample(samp, tc + 2 * dx).rgb;
+    float3 c02 = tex.Sample(samp, tc - dx + dy).rgb;
+    float3 c12 = tex.Sample(samp, tc + dy).rgb;
+    float3 c22 = tex.Sample(samp, tc + dx + dy).rgb;
+    float3 c32 = tex.Sample(samp, tc + 2 * dx + dy).rgb;
+    float3 c03 = tex.Sample(samp, tc - dx + 2 * dy).rgb;
+    float3 c13 = tex.Sample(samp, tc + 2 * dy).rgb;
+    float3 c23 = tex.Sample(samp, tc + dx + 2 * dy).rgb;
+    float3 c33 = tex.Sample(samp, tc + 2 * dx + 2 * dy).rgb;
 
     float3 color = mul(weights[0], float4x3(c00, c10, c20, c30));
     color += mul(weights[1], float4x3(c01, c11, c21, c31));
